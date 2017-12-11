c8559eb29dda803264cf5b8e06b479676f5ceb71
SIPU <ddasik00@naver.com>
2017-12-11 18:43:01 +0900

    .

--- JavaScript DOM/codewar.js
+++ JavaScript DOM/codewar.js
@@ -659,3 +659,20 @@ var typer = (function() {
     isNull:      function (x) { return x === null }
   };
 }());
+
+
+//longest common sequence
+function LCS( xstr, ystr ) {
+  if( xstr == '' || ystr == '' ) return '';
+    
+  var xp    = xstr.charAt( 0 ), xrest = xstr.slice( 1 ),
+      yp    = ystr.charAt( 0 ), yrest = ystr.slice( 1 );
+      
+  if( xp == yp )
+    return xp + LCS( xrest, yrest );
+  
+  var lcs1 = LCS( xstr, yrest ),
+      lcs2 = LCS( xrest, ystr );
+      
+  return (lcs1.length > lcs2.length) ? lcs1 : lcs2;
+}
