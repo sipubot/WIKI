5a73bf93b0034968e429768b330685104cef3755
SIPU <ddasik00@naver.com>
2017-12-11 17:24:52 +0900

    ..

--- JavaScript DOM/codewar.js
+++ JavaScript DOM/codewar.js
@@ -642,4 +642,20 @@ function evilTwin(obj) {
       value: true
     }
   });
-}
+}/
+
+//type checker
+var typer = (function() {
+  return {
+    isUndefined: function (x) { return typeof x == "undefined" },
+    isFunction:  function (x) { return typeof x == "function" },
+    isNumber:    function (x) { return typeof x != "undefined" && typeof x.valueOf() == "number" && !isNaN(x.valueOf()) },
+    isString:    function (x) { return typeof x != "undefined" && typeof x.valueOf() == "string" },
+    isBoolean:   function (x) { return typeof x != "undefined" && typeof x.valueOf() == "boolean" },
+    isArray:     function (x) { return x instanceof Array },
+    isDate:      function (x) { return x instanceof Date },
+    isRegExp:    function (x) { return x instanceof RegExp },
+    isError:     function (x) { return x instanceof Error },
+    isNull:      function (x) { return x === null }
+  };
+}());
