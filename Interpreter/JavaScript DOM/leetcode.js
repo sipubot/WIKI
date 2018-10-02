8d30ec888c9638c146f145c26790645b5d7ad3d7
Sipubot <ddasik00@naver.com>
2018-10-03 03:49:50 +0900

    하루 1문제

--- Interpreter/JavaScript DOM/leetcode.js
+++ Interpreter/JavaScript DOM/leetcode.js
@@ -9,3 +9,38 @@ var singleNumber = function(nums) {
     return nums.reduce((x, y) => x ^ y);
 };
   //비트 단위 배타적 논리합 연산 대입 	x ^= y 	x = x ^ y
+
+// https://leetcode.com/submissions/detail/179993117/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root == null) return 0;
+  
+  var maxdep = 0;
+  function dep (n, node) {
+      if (node == null) {
+      } else {
+          maxdep =  maxdep > n ? maxdep : n;
+          if (node.left !== null) {
+              dep (n+1, node.left);
+          }
+          if (node.right !== null) {
+              dep (n+1, node.right);
+          }
+          
+      }
+  }
+  dep(0,root);
+  
+  return maxdep+1;
+};
