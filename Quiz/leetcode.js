75bb82bef63367dc750b0d726f0cd13554f34678
Sipubot <ddasik00@naver.com>
2018-11-06 20:22:01 +0900

    ..

--- Quiz/leetcode.js
+++ Quiz/leetcode.js
@@ -413,3 +413,53 @@ var isMonotonic = function(A) {
     }
     return re;
 };
+
+//https://leetcode.com/submissions/detail/187834928/
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.stack = [];
+    this.min = [];
+  };
+  
+  /**
+   * @param {number} x
+   * @return {void}
+   */
+  MinStack.prototype.push = function(x) {
+    this.stack.push(x);
+  
+    var min = this.getMin();
+    if (min !== undefined) {
+      this.min.push(Math.min(x, min));
+    } else {
+      this.min.push(x);
+    }
+  };
+  
+  /**
+   * @return {void}
+   */
+  MinStack.prototype.pop = function() {
+    this.stack.pop();
+    this.min.pop();
+  };
+  
+  /**
+   * @return {number}
+   */
+  MinStack.prototype.top = function() {
+    if (this.stack.length > 0) {
+      return this.stack[this.stack.length - 1];
+    }
+  };
+  
+  /**
+   * @return {number}
+   */
+  MinStack.prototype.getMin = function() {
+    if (this.min.length > 0) {
+      return this.min[this.min.length - 1];
+    }
+  };
