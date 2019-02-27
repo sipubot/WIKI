f31394497181c54256cdc1a84e76e05f287f79ac
SIPU <ddasik00@naver.com>
2019-02-27 17:49:41 +0900

    Create leetcode.rs

---
+++ Quiz/leetcode.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+        let mut t: i32 = 0 as i32;
+        let mut re: i32 = 0 as i32;
+        for num in nums {
+            if num == 1 as i32 {
+                t = t + 1;
+                if &t > &re {
+                    re = &t;
+                }
+            } else {
+                t = 0 as i32;
+            }
+        }
+        re
+    }
+}
