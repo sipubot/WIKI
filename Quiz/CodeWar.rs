8fe9dd8322b108e54502693a276464fcfc069050
Sipubot <ddasik00@naver.com>
2019-03-01 00:58:43 +0900

    ..

---
+++ Quiz/CodeWar.rs
@@ -0,0 +1,14 @@
+fn abbrev_name(name: &str) -> String {
+    // code away
+    let mut re: Vec<String> = vec![];
+    for a in name.split_whitespace() {
+        match a.chars().next()  {
+            Some(x) => {
+                re.push(x.to_string().to_uppercase());
+            },
+            None => {}
+        }
+    }
+    re.join(".")
+    
+}
