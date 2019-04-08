15c52a78fe1beb98d1cc22a1eb3913a63e1780be
SIPU <ddasik00@naver.com>
2019-04-08 10:05:25 +0900

    Update use interactive

--- Quiz/CodeJamPlate.js
+++ Quiz/CodeJamPlate.js
@@ -1,58 +1,52 @@
 'use strict'
-//init
-const PS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 /***
  * solve
  */
 function solve(v) {
-    const arr = v.PartyList;
-    const max = v.PartyMax;
-    var re = '';
-    for (var i = max; i > 0; i--) {
-        for (var j = 0; j < arr.length; j++) {
-            if (arr[j] >= i) {
-                re += PS[j];
-            }
+}
+var status = 'lo_hi'
+function IAprocess(line) {
+    if (status === 'lo_hi') {
+        lo_hi = line.split(' ');
+        head = parseInt(lo_hi[0]) + 1;
+        tail = parseInt(lo_hi[1]);
+        status = 'num_tries';
+    } else if (status === 'num_tries') {
+        num_tries = line; // not used.
+        expect = 'solve';
+        mid = parseInt((head + tail) / 2);
+        console.log(mid);
+    } else if (status === 'solve') {
+        if (line === 'CORRECT') {
+            status = 'lo_hi';
+            return true;
+        } else {
+            line === 'TOO_SMALL' ? head = mid + 1 : tail = mid - 1;
+            mid = parseInt((head + tail) / 2);
+            console.log(mid);
         }
     }
-    if (re.length % 2 == 1) {
-        var a = [];
-        a.push(re[0]);
-        re = re.substr(1, re.length - 1)
-        a = a.concat(re.match(/.{1,2}/g));
-        return a.join(' ');
-    } else {
-        return re.match(/.{1,2}/g).join(' ');
-    }
-}
-//ex> solve({PartyList:[3,1,1,4],PartyMax:4})
-function checkResult(v) {
-    return true;
+    return false;
 }
 // CaseParser
 class CaseParser {
     constructor(caseNumber) {
         this.caseNo = caseNumber
 
-        this.PartyCount = 0
-        this.PartyList = []
-        this.PartyMax = 0
+        this.init = 0
 
-        this.state = 'party'
+        this.state = 'init'
     }
     readline(line) {
         switch (this.state) {
-            case 'party': {
+            case 'init': {
 
-                this.PartyCount = parseInt(line)
+                this.init = parseInt(line)
+
                 this.state = 'rows'
                 break;
             }
             case 'rows': {
-
-                this.PartyList = line.split(' ').map(a => +a);
-                this.PartyMax = Math.max(...this.PartyList);
-
                 //get all case value state change Done
                 this.state = 'done'
                 break;
@@ -64,44 +58,11 @@ class CaseParser {
     }
     getCase() {
         return {
-            PartyList: this.PartyList,
-            PartyMax: this.PartyMax
+            init: this.init
         }
     }
 }
 // InteractiveResultParser
-class IAParser {
-    constructor(caseNumber) {
-        this.caseNo = caseNumber
-
-        this.Result = ""
-
-        this.state = 'onreceive'
-    }
-    readline(line) {
-        switch (this.state) {
-            case 'onreceive': {
-                this.Result = line;
-                //this.state = 'next1'
-                this.state = 'done';
-                break;
-            }
-            case 'next1': {
-                //get all case value state change Done
-                this.state = 'done'
-                break;
-            }
-        }
-    }
-    isComplete() {
-        return (this.state === 'done')
-    }
-    getResult() {
-        return {
-            Result: this.Result
-        }
-    }
-}
 /***
  * Core Line When fix at Comlex Problem
  */
@@ -113,7 +74,7 @@ class ProblemParser {
         this.cases = []
         this.casesResult = []
         this.state = 't'
-        //init first Case with number 1
+        ////init first Case with number 1
         this.caseParser = new CaseParser(1)
         this.caseResultParser = null
     }
@@ -122,8 +83,8 @@ class ProblemParser {
             case 't': {
                 this.t = parseInt(line)
                 //this.state = 'caseall'
-                this.state = 'casebycase'
-                //this.state = 'interaction'
+                //this.state = 'casebycase'
+                this.state = 'interaction'
                 break;
             }
             case 'caseall': {
@@ -148,36 +109,17 @@ class ProblemParser {
             }
             case 'interaction': {
                 //case by case interactive
-                this.caseParser.readline(line);
-                if (this.caseParser.isComplete()) {
-                    console.log(solve(this.caseParser.getCase()));
-                    this.state = 'interactionResult';
-                    this.caseResultParser = new IAParser(1 + this.currentT);
-                }
-            }
-            case 'interactionResult': {
-                //case by case interactive
-                this.caseResultParser.readline(line);
-                if (this.caseResultParser.isComplete()) {
-                    ////add interactive result push
-                    //this.casesResult.push(this.caseResultParser.getResult());
-                    ////check case pass interaction only
-                    if (checkResult(this.caseResultParser.getResult())) {
-                        this.currentT += 1;
-                    }
-                    ////when alway counting Test
-                    //this.currentT += 1;
-                    this.state = 'interaction';
-                    this.caseParser = new CaseParser(this.currentT + 1);
+                if (IAprocess(line)) {
+                    this.currentT += 1
                 }
             }
         }
         if (this.currentT === this.t) {
-            this.state = 'done'
+            this.state += '_done';
         }
     }
     isComplete() {
-        return (this.state === 'done')
+        return this.state.indexOf('done') > -1
     }
     getResult() {
         return this.casesResult
@@ -208,10 +150,16 @@ function main() {
             rl.close()
         }
     }).on('close', () => {
-        ////processing all case
-        //printResult(solve(problemParser.getCases()));
-        ////just printing Result
-        printResult(problemParser.getResult());
+        switch (problemParser.state) {
+            case 'caseall_done': {
+                printResult(solve(problemParser.getCases()));
+            }
+                break;
+            case 'casebycase_done': {
+                printResult(problemParser.getResult());
+            }
+                break;
+        }
         process.exit(0)
     });
 }
