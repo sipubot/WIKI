fdb29eec189f0379ad31bdb7312da173791534be
SIPU <ddasik00@naver.com>
2019-04-27 13:09:56 +0900

    Update Codewar.js

--- Quiz/Codewar.js
+++ Quiz/Codewar.js
@@ -1,3 +1,36 @@
+//https://www.codewars.com/kata/the-observed-pin/train/javascript
+function getPINs(observed) {
+  // TODO: This is your job, detective!
+  var padd = {
+    "1" : ["1","2","4"],
+    "2" : ["1","2","3","5"],
+    "3" : ["2","3","6"],
+    "4" : ["1","4","5","7"],
+    "5" : ["2","4","5","6","8"],
+    "6" : ["3","5","6","9"],
+    "7" : ["4","7","8"],
+    "8" : ["5","7","8","9","0"],
+    "9" : ["6","8","9"],
+    "0" : ["8","0"]
+  }
+  var ss = observed.split('');
+  var karr = [];
+  var re = new Set();
+  ss.map(a=>{
+    karr.push(padd[a]);
+  });
+  function kcom(arr, idx, kc) {
+    if (idx === arr.length) {
+      re.add(kc);
+    } else {
+      for(var i = 0; i < karr[arr[idx]].length; i++) {
+        kcom(arr, idx+1, kc+karr[arr[idx]][i])        
+      }
+    }
+  }
+  kcom([...Array(ss.length)].map((a,i)=>i),0,"");
+  return Array.from(re);
+}
 //https://www.codewars.com/kata/simple-fun-number-395-fibonacci-digit-sequence/train/javascript
 function find(a,b,n){
   //coding and coding..
