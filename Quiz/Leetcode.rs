81b4c91a5199d424b64c5cf5ffb5917faa3b0d36
SIPU <ddasik00@naver.com>
2019-05-07 15:25:50 +0900

    Update Leetcode.rs

--- Quiz/Leetcode.rs
+++ Quiz/Leetcode.rs
@@ -1,4 +1,26 @@
 impl Solution {
+    pub fn longest_valid_parentheses(s: String) -> i32 {
+        let mut stack = Vec::new();
+        let mut max = 0;
+
+        stack.push(-1);
+        for (i, c) in s.chars().enumerate() {
+            if c == '(' {
+                stack.push(i as i32);
+            } else {
+                stack.pop();
+                if stack.is_empty() {
+                    stack.push(i as i32);
+                } else {
+                    let l = i as i32 - stack[stack.len() - 1];
+                    if l > max {
+                        max = l;
+                    }
+                }
+            }
+        }
+        max
+    }
     pub fn subdomain_visits(cpdomains: Vec<String>) -> Vec<String> {
         use std::collections::HashMap;
         let mut subdomain_counts: HashMap<String, i32> = HashMap::new();
